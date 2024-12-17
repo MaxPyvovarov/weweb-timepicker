83728068c7752f0eb3c2ee700faef4a16bd9d3a0
Maksym Pyvovarov <mpyvovarov@sapi-tech.com>
2024-12-17 12:35:31 +0200

    Initial commit

---
+++ ww-config.js
@@ -0,0 +1,16 @@
+export default {
+	editor: {
+		label: {
+			en: 'Custom Time Picker',
+		},
+	},
+	properties: {
+		textColor: {
+			label: {
+				en: 'Text color',
+			},
+			type: 'Color',
+			defaultValue: '#F23636',
+		},
+	},
+};
